be5810437f913c5e6d7bc5873e2f6c2d830eb046
Ibhafidon ehiremen francis <Ehiremengold@gmail.com>
2019-04-06 16:50:42 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 from random import choice 
-choicer = ['good to chat with you','that is nice','you will be alright', 'okay']
+choicer = ['good to chat with you','that is nice','you will be alright', 'okay','lol', 'that\'s good to know', 'wow!', 'lmao', 'ode', 'ozwor','got it']
 print('q-bot: Hi')
 user_input1 = input("You:")
 print('q-bot: This is q-bot! nice to meet you')
@@ -12,10 +12,92 @@ print('q-bot: Nice to meet you '+ user_input2 +'!')
 print('q-bot:How was your day '+ user_input2)
 user_input3 = input('You:')
 if user_input3 == "good":
-   print('That great ' + user_input2 + "," + ' that\'s good to know.')
+   print('q-bot:That great ' + user_input2 + "," + ' that\'s good to know.')
 elif user_input3 == 'bad':
-   print('Awwwn, you will be alright.')
+   print('q-bot:Awwwn, you will be alright.')
 else:
-   print(choice(choicer))
- 
+   print('q-bot:'+ choice(choicer))
+user_input4 = input('You:')
+if user_input4 == 'how are you':
+  print('q-bot:I am fine thanks for asking')
+elif user_input4 == 'who are you':
+  print('q-bot:I am a python chatbot created by queen-tech')
+elif user_input4 == 'how old are you':
+  print('q-bot:Lol i think you would definetely be older than me')
+elif user_input4 == 'what is your name':
+  print('q-bot:my name is q-bot!')
+elif user_input4 == 'are you human':
+  print('q-bot:lol '+ user_input2 + ' you are funny, definitely i am not')
+elif user_input4 == 'what\'s your name':
+  print('q-bot:my name is q-bot!')
+elif user_input4 == 'who made you':
+  print('q-bot:Ehis made me, i believe God made you.')
+elif user_input4 == 'are you still there':
+  print('q-bot:Yes, anytime!')
+elif user_input4 == 'are you a boy or a girl':
+  print('q-bot:I am neither male nor female')
+else:
+  print('Wow,really')
+
+user_input5 = input('You:')
+if user_input5 == 'how are you':
+  print('q-bot:I am fine thanks for asking')
+elif user_input5 == 'who are you':
+  print('q-bot:I am a python chatbot created by queen-tech')
+elif user_input5 == 'how old are you':
+  print('q-bot:Lol i think you would definetely be older than me')
+elif user_input5 == 'what is your name':
+  print('q-bot:my name is q-bot!')
+elif user_input5 == 'are you human':
+  print('q-bot:lol '+ user_input2 + ' you are funny, definitely i am not')
+elif user_input5 == 'what\'s your name':
+  print('q-bot:my name is q-bot!')
+elif user_input5 == 'who made you':
+  print('q-bot:Ehis made me, i believe God made you.')
+elif user_input5 == 'are you still there':
+  print('q-bot:Yes, anytime!')
+elif user_input5 == 'are you a boy or a girl':
+  print('q-bot:I am neither male nor female')
+else:
+  print('Wow,really')
+  
+user_input6 = input('You:')
+if user_input6 == 'how are you':
+  print('q-bot:I am fine thanks for asking')
+elif user_input6 == 'who are you':
+  print('q-bot:I am a python chatbot created by queen-tech')
+elif user_input6 == 'how old are you':
+  print('q-bot:Lol i think you would definetely be older than me')
+elif user_input6 == 'what is your name':
+  print('q-bot:my name is q-bot!')
+elif user_input6 == 'are you human':
+  print('q-bot:lol '+ user_input2 + ' you are funny, definitely i am not')
+elif user_input6 == 'what\'s your name':
+  print('q-bot:my name is q-bot!')
+elif user_input6 == 'who made you':
+  print('q-bot:Ehis made me, i believe God made you.')
+elif user_input6 == 'are you still there':
+  print('q-bot:Yes, anytime!')
+elif user_input6 == 'are you a boy or a girl':
+  print('q-bot:I am neither male nor female')
+else:
+  print('Wow,really')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
